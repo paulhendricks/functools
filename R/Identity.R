88307d854be06aefb6c650ea30c0974e6958cb64
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-09 18:57:29 +0000

    Starting to create new .R files for functions

---
+++ R/Identity.R
@@ -0,0 +1,15 @@
+#' Identity
+#'
+#' \code{Identity()} returns itself.
+#'
+#' @param x an object.
+#' @return the object.
+#' @examples
+#' # Return itself:
+#' Identity(5)
+#' Identity(mean)
+#' Identity(lm(data = mtcars, mpg ~ cyl))
+#'
+Identity <- function(x) {
+  return(x)
+}
--- R/functools.R
+++ R/functools.R
@@ -4,22 +4,6 @@
 #' @name functools
 NULL
 
-#' Identity
-#'
-#' \code{Identity()} returns itself.
-#'
-#' @param x an object.
-#' @return the object.
-#' @examples
-#' # Return itself:
-#' Identity(5)
-#' Identity(mean)
-#' Identity(lm(data = mtcars, mpg ~ cyl))
-#'
-Identity <- function(x) {
-  return(x)
-}
-
 #' True
 #'
 #' \code{True()} is a function that returns TRUE.
