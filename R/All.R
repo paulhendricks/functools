47c95af183da8bc700b089645fa701cf7b3c84de
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-10 18:26:52 +0000

    All() function

---
+++ R/All.R
@@ -0,0 +1,27 @@
+#' All
+#'
+#' \code{All()}
+#'
+#' @param x an object.
+#' @param f a predicate function.
+#' @return a logical value.
+#' @examples
+#' # comment here
+#' All(is.numeric, mtcars) # TRUE
+#' All(is.character, mtcars) # FALSE
+#' mtcars$am <- factor(mtcars$am)
+#' All(is.numeric, mtcars) # FALSE
+#' All(is.factor, mtcars) # FALSE
+#'
+#' # Handles NAs and NULLs
+#' All(is.numeric, list(NA, "3", NULL)) # FALSE
+#' All(is.numeric, list(NA, 3, NULL)) # FALSE
+#' All(is.numeric, list(NA, "3", NULL, 5)) # FALSE
+#'
+#' # Use na.rm = TRUE to remove NULLS
+#' All(Identity, list(NA, TRUE)) # NA
+#' All(Identity, list(NA, TRUE), na.rm = TRUE) # TRUE
+All <- function(f, x, ..., na.rm = FALSE) {
+  force(f); f <- match.fun(f)
+  return(all(vapply(x, f, logical(1), ...), na.rm = na.rm))
+}
---
+++ tests/testthat/test-All.R
@@ -0,0 +1,13 @@
+library(functools)
+context("Testing All() function")
+
+test_that("All() returns TRUE if a predicate function applied over an iterable returns all TRUE values", {
+  expect_equal(All(is.numeric, mtcars), TRUE)
+  expect_equal(All(is.character, mtcars), FALSE)
+  #   expect_equal(All(All), TRUE)
+  expect_equal(All(is.numeric, list(NA, "3", NULL)), FALSE)
+  expect_equal(All(is.numeric, list(NA, "3", NULL, 5)), FALSE)
+  expect_equal(All(is.numeric, list(NA, 3, NULL)), FALSE)
+  expect_equal(All(Identity, list(NA, TRUE)), NA)
+  expect_equal(All(Identity, list(NA, TRUE), na.rm = TRUE), TRUE)
+})
