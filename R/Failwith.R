9f2d1cbfa002134f6d564b59bd1f3f40c93a8908
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-10 18:27:37 +0000

    added Failwith() function

---
+++ R/Failwith.R
@@ -0,0 +1,23 @@
+#' Failwith
+#'
+#' \code{Failwith()} turns a function that throws an error into a function that returns a default value when there’s an error.
+#' The essence of Failwith() is simple; it’s just a wrapper around try(), the function that captures errors and allows execution to continue.
+#'
+#' @param default default value.
+#' @param f any function that throws an error.
+#' @param silent logical: should the report of error messages be suppressed?.
+#' @return a function that returns a default value when there's an error.
+#' @examples
+#' Some examples:
+#'
+#'
+#'
+#'
+Failwith <- function(default = NULL, f, silent = FALSE) {
+  force(f); f <- match.fun(f)
+  return(function(...) {
+    out <- default
+    try(out <- f(...), silent = silent)
+    return(out)
+  })
+}
---
+++ tests/testthat/test-Failwith.R
@@ -0,0 +1,7 @@
+library(functools)
+context("Testing Failwith() function")
+
+new_log <- Failwith(0, log)
+test_that("Failwith() does something", {
+#   expect_equal()
+})
