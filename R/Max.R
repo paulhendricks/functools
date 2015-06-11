3f33d949e2008d8f0819066751b524bf225d8976
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-11 13:59:43 +0000

    Added Max() function

---
+++ R/Max.R
@@ -0,0 +1,17 @@
+#' Max
+#'
+#' \code{Failwith()} turns a function that throws an error into a function that returns a default value when there’s an error.
+#' The essence of failwith() is simple; it’s just a wrapper around try(), the function that captures errors and allows execution to continue.
+#'
+#' @param f a predicate function.
+#' @return  the negation of that function.
+#' @examples
+#' Some examples:
+#'
+#'
+#'
+#'
+Max <- function(x, f) {
+  force(f); f <- match.fun(f)
+  return(1L) # Placeholder
+}
