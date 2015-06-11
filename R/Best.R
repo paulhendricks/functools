ddcfbe4f2c735725ba682278855ea10b09d243c6
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-11 13:59:09 +0000

    Added Finder() function

---
+++ R/Best.R
@@ -0,0 +1,20 @@
+#' Best
+#'
+#' \code{Best()} takes a vector and a two-argument function that returns a logical constant (either TRUE or FALSE),
+#' and returns the that function reduced over that vector.
+#'
+#' @param x a vector.
+#' @return  a two-argument function that returns a logical constant.
+#' @examples
+#' # Simulate the behavior of max with numerics
+#' Best(1:10, function(x, y) return(x > y))
+#' # Simulate the behavior of min with numerics
+#' Best(1:10, function(x, y) return(x < y))
+#' # This comparison function prefers values that begin with l
+#' Best(letters, function(x, y) return(x[1] == "l"))
+Best <- function(x, f) {
+  force(f); f <- match.fun(f)
+  return(Reduce(function(x, y) {
+    return(ifelse(f(x, y), x, y))
+  }, x))
+}
