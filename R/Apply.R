68f04137f4f68bf3dc7d02ae5e1f70b1157b61f6
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-11 13:58:40 +0000

    Added Apply() function

---
+++ R/Apply.R
@@ -0,0 +1,17 @@
+#' Apply
+#'
+#' \code{Apply}
+#'
+#' @param f a function.
+#' @param check a function.
+#' @param init a valie.
+#' @return a list.
+#' @examples
+#' Some examples:
+#'
+#'
+#'
+#'
+Apply <- function(f, x, margin, ...) {
+  return(apply(X = x, MARGIN = margin, FUN = f, ...))
+}
