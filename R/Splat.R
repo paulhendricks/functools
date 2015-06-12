6f403c0f3577f1bdf5c42562d1552aa5528c5c58
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-12 13:30:49 +0000

    Added Splat() function

---
+++ R/Splat.R
@@ -0,0 +1,19 @@
+#' Splat
+#'
+#' \code{negate()} takes a function that returns a logical vector (a predicate function), and returns the negation of that function.
+#' This can be a useful shortcut when a function returns the opposite of what you need.
+#'
+#' @param f a predicate function.
+#' @return  the negation of that function.
+#' @examples
+#' Some examples:
+#'
+#'
+#'
+#'
+Splat <- function(f) {
+  force(f); f <- match.fun(f)
+  return(function(vector) {
+    return(f(vector))
+  })
+}
