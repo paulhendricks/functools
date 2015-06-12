bcdc3561b4207c140ecda60d1bec0fc6815a4175
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-12 13:28:53 +0000

    Added Repeatedly() function

---
+++ R/Repeatedly.R
@@ -0,0 +1,17 @@
+#' Repeatedly
+#'
+#' \code{Repeat()} turns a function that throws an error into a function that returns a default value when there’s an error.
+#' The essence of failwith() is simple; it’s just a wrapper around try(), the function that captures errors and allows execution to continue.
+#'
+#' @param f a predicate function.
+#' @return  the negation of that function.
+#' @examples
+#' Some examples:
+#'
+#'
+#'
+#'
+Repeatedly <- function(f, x) {
+  force(f); f <- match.fun(f)
+  return(1L) # Placeholder
+}
