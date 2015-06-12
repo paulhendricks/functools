9238e141a75fdeb420ab8b1f8b1a6906ae196904
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-12 13:30:33 +0000

    Updated Compose() with infix operator

--- R/Compose.R
+++ R/Compose.R
@@ -14,3 +14,18 @@ Compose <- function(f, g) {
   force(g); g <- match.fun(g)
   return(function(...) f(g(...)))
 }
+
+#' Compose Operator
+#'
+#' %notin% is a more intuitive interface as a binary operator, which returns a logical vector indicating if there is a match or not for its left operand.
+#'
+#' @param x vector or NULL: the values to be matched. Long vectors are supported.
+#' @param table vector or NULL: the values to be matched against. Long vectors are not supported
+#' @return A vector of the same length as x.
+#' @examples
+#' # Some examples
+#' "bc" %notin% letters # TRUE
+#' "b" %notin% letters # FALSE
+#' "c" %notin% letters # FALSE
+#'
+`%o%` <- Compose
