86f4c99f9059246b05a800b81234f940328ddf0e
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-13 21:08:34 +0000

    Added unit tests for Any()

--- tests/testthat/test-Any.R
+++ tests/testthat/test-Any.R
@@ -1,12 +1,17 @@
 library(functools)
-context("Testing Any() function")
+context("Any()")
 
-test_that("Any() returns TRUE if a predicate function applied over an iterable returns any TRUE values", {
+test_that("Any() returns TRUE if a predicate function applied over an iterable returns any TRUE values.", {
   expect_equal(Any(is.numeric, mtcars), TRUE)
   expect_equal(Any(is.character, mtcars), FALSE)
   #   expect_equal(Any(Any), TRUE)
   expect_equal(Any(is.numeric, list(NA, "3", NULL)), FALSE)
   expect_equal(Any(is.numeric, list(NA, "3", NULL, 5)), TRUE)
+  expect_equal(Any(is.numeric, list(NA, 3, NULL)), TRUE)
   expect_equal(Any(Identity, list(NA, FALSE)), NA)
   expect_equal(Any(Identity, list(NA, FALSE), na.rm = TRUE), FALSE)
+})
+
+test_that("Any() returns an atomic vector of type logical.", {
+  expect_equal(1, 1)
 })
