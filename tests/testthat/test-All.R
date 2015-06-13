1fe1ade3968d6d7fd935f9bf26038208aa250ed0
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-13 21:08:11 +0000

    Added unit test boilerplate for All()

--- tests/testthat/test-All.R
+++ tests/testthat/test-All.R
@@ -1,7 +1,7 @@
 library(functools)
 context("All()")
 
-test_that("All() returns TRUE if a predicate function applied over an iterable returns all TRUE values", {
+test_that("All() returns TRUE if a predicate function applied over an iterable returns all TRUE values.", {
   expect_equal(All(is.numeric, mtcars), TRUE)
   expect_equal(All(is.character, mtcars), FALSE)
   #   expect_equal(All(All), TRUE)
@@ -11,3 +11,9 @@ test_that("All() returns TRUE if a predicate function applied over an iterable r
   expect_equal(All(Identity, list(NA, TRUE)), NA)
   expect_equal(All(Identity, list(NA, TRUE), na.rm = TRUE), TRUE)
 })
+
+test_that("All() returns an atomic vector of type logical.", {
+  expect_equal(1, 1)
+})
+
+
