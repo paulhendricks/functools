aaf0fae9563a83e69411013ae59dab6af822a01c
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-15 21:26:40 +0000

    Updated unit test for Compose()

--- tests/testthat/test-Compose.R
+++ tests/testthat/test-Compose.R
@@ -1,7 +1,12 @@
 library(functools)
-context("Testing Compose() function")
+context("Compose()")
 
 lu <- Compose(length, unique)
 test_that("Compose() returns a function composed of two functions", {
   expect_equal(lu(c(1:10, 5:15, 20:25)), 21)
+})
+
+III <- Compose(Identity, Identity, Identity)
+test_that("Compose() returns a function", {
+  expect_is(III, "function")
 })
