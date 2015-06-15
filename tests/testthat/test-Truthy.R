90d235f8831742c09f60f8ecc0fc02cacc68a851
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-15 22:01:59 +0000

    Updated unit test

--- tests/testthat/test-Truthy.R
+++ tests/testthat/test-Truthy.R
@@ -1,10 +1,10 @@
 library(functools)
-context("Testing Truthy() function")
+context("Truthy()")
 
 test_that("Truthy() returns TRUE if an object is 'true'", {
   expect_equal(Truthy(NULL), FALSE)
   expect_equal(Truthy(NA), FALSE)
-  #   expect_equal(Truthy(Truthy), TRUE)
+  expect_warning(Truthy(Truthy), "is.na() applied to non-(list or vector) of type 'closure'", fixed = TRUE)
   expect_equal(Truthy(1), FALSE)
   expect_equal(Truthy(0), FALSE)
   expect_equal(Truthy(2.4), FALSE)
