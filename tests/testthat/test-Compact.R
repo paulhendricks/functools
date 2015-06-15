82ff166a226c7da63a1afaa74ccaabb0f86a7af8
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-15 21:26:32 +0000

    Updated unit test for Compact()

--- tests/testthat/test-Compact.R
+++ tests/testthat/test-Compact.R
@@ -1,7 +1,10 @@
 library(functools)
-context("Testing Compact() function")
+context("Compact()")
 
-foo <- list(NULL, 1, 5, NULL)
-test_that("Compact removes NULLs from lists", {
-  expect_equal(Compact(foo), list(1, 5))
+foo <- list(NA_integer_, TRUE, NA_character_, 1, NULL,
+            "a", NA_complex_, 4.5, FALSE, NA_real_)
+bar <- list(TRUE, 1, "a", 4.5, FALSE)
+test_that("Compact removes NULLs and NAs from lists", {
+  expect_equal(Compact(bar), bar)
+  expect_equal(Compact(foo), bar)
 })
