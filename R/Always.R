54a3600ff9273542cce38d4230d5958577f5d6ad
paulhendricks <paul.hendricks.2013@owu.edu>
2015-06-16 13:44:24 +0000

    Built an Always() function

---
+++ R/Always.R
@@ -0,0 +1,17 @@
+#' Always
+#'
+#' Always(x) is a closure function that always returns a function returning object x.
+#'
+#' @param x an object.
+#' @return a function that itself returns x.
+#' @examples
+#' # comment here
+#' always_0 <- Always(0)
+#' always_0() # 0
+#' always_true <- Always(TRUE)
+#' always_true() # TRUE
+#'
+#'
+Always <- function(x) {
+  return(function() {return(x)})
+}
