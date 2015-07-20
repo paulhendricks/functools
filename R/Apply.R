9ab2b7b870d440f9c73bd8b047f9f4bfccdedfca
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-20 20:30:29 +0000

    update to Apply() documentation

--- R/Apply.R
+++ R/Apply.R
@@ -1,17 +1,12 @@
 #' Apply
 #'
-#' \code{Apply}
+#' Wrapper for \code{\link[base]{apply}}.
 #'
 #' @param f the function to be applied.
 #' @param x an array, including a matrix.
 #' @param margin a vector giving the subscripts which the function will be applied over.
 #' @param ... optional arguments to f.
-#' @return something.
-#' @examples
-#' # Some examples
-#'
-#'
-#'
+#' @seealso \code{\link[base]{apply}} for code and documentation.
 #' @export
 Apply <- function(f, x, margin, ...) {
   return(apply(X = x, MARGIN = margin, FUN = f, ...))
