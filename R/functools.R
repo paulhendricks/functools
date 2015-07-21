03cb7fb770728b4290b3de3514bd914117cccde5
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-21 14:48:30 +0000

    updates to documentation and version!

--- R/functools.R
+++ R/functools.R
@@ -1,15 +1,19 @@
-#' functools: An R package for extending functional programming in R.
+#' functools
 #'
-#' functools is a utility-belt library for R that adds support to the usual
-#' higher order functional suspects (Map, Reduce, Filter, etc.) without
-#' extending any core R objects.
-#'
-#' It has three main types of helper functions:
+#' functools is an R package for extending functional programming in R. It has three main goals:
 #'
 #' \itemize{
-#' \item Closures
-#' \item Functionals
-#' \item Function Operators
+#' \item Add support to the usual higher order functional suspects (Map, Reduce, Filter, etc.) without extending any core R objects.
+#' \item Provide blazing fast performance for in-memory data by writing key pieces in C++ and options for parallelization, where possible.
+#' \item Use a consistent API to access different functionals in base R such as `lapply` or `apply`.
+#' }
+#'
+#' functools achieves these goals through three main types of function design patterns:
+#'
+#' \itemize{
+#' \item Closures (functions that take data and return functions)
+#' \item Functionals (functions that take functions and return data)
+#' \item Function Operators (functions that take functions and return functions)
 #' }
 #'
 #' To learn more about functools, start with the vignettes:
