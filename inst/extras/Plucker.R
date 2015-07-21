a7624701f29e92b61f6e61ec88006bd3d765cc9a
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-21 14:30:58 +0000

    moved functions not yet built to a temporary folder

--- inst/extras/Plucker.R
+++ inst/extras/Plucker.R
@@ -1,9 +1,12 @@
-#' Plucker
+#' Create a function that plucks a field from S3 and S4 objects.
 #'
-#' Pluck objects from S3 and S4 object slots!
+#' \code{Plucker} is a convenience function that when passed a character
+#' vector as an argument, will return a function that "plucks" objects
+#' from S3 and S4 object slots!
 #'
 #' @param field a character vector.
-#' @return  something.
+#' @return  a function that plucks fields from S3 and S4 object slots.
+#' @family closures
 #' @examples
 #' # Pluck a value from a list or S3 object:
 #' new_model <- lm(mtcars, formula = hp ~ wt)
