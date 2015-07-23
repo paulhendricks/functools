3a00c00b352f1d0b620ccae269f005ab2bf77c83
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-23 13:44:32 +0000

    important functions to standardize

---
+++ inst/extras/Sapply.R
@@ -0,0 +1,18 @@
+#' Sapply
+#'
+#' \code{Sapply()}
+#'
+#' @param f a predicate function.
+#' @param x a value.
+#' @return  something.
+#' @family aggregate functionals
+#' @examples
+#' # Some examples
+#'
+#'
+#'
+#' @export
+Sapply <- function(f, x) {
+  f <- match.fun(f)
+  return(1L) # Placeholder
+}
---
+++ inst/extras/Tapply.R
@@ -0,0 +1,18 @@
+#' Tapply
+#'
+#' \code{Tapply()}
+#'
+#' @param f a predicate function.
+#' @param x a value.
+#' @return  something.
+#' @family aggregate functionals
+#' @examples
+#' # Some examples
+#'
+#'
+#'
+#' @export
+Tapply <- function(f, x) {
+  f <- match.fun(f)
+  return(1L) # Placeholder
+}
