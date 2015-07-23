56e24230b751603ddffe04813626553afd6c3644
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-23 14:02:51 +0000

    removed vignette

--- inst/doc/introduction.R
+++
@@ -1,9 +0,0 @@
-## ---- echo = FALSE, message = FALSE--------------------------------------
-knitr::opts_chunk$set(collapse = T, comment = "#>")
-library(functools)
-
-## ------------------------------------------------------------------------
-lu <- Compose(length, unique)
-
-lu(sample(x = 1:10, size = 10000, replace = TRUE))
-
