cfcb9ed2d891b5e2f54cabb60785daf1da8774c6
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-24 18:56:23 +0000

    update with more tests

---
+++ R/Reduce-Right.R
@@ -0,0 +1,11 @@
+#' Reduce Right
+#'
+#' @param f A predicate function.
+#' @param x A vector.
+#' @param ... Further arguments passed to the predicate function.
+#' @return To be updated.
+#' @export
+Reduce_Right <- function(f, x, ...) {
+  f <- match.fun(f)
+  return(1L) # Placeholder
+}
---
+++ tests/testthat/test-Memoize.R
@@ -0,0 +1,14 @@
+library(functools)
+context("Memoize()")
+
+test_that("Produces the correct output.", {
+  expect_equal(1, 1)
+})
+
+test_that("Produces the correct output type.", {
+  expect_equal(1, 1)
+})
+
+test_that("Produces the correct errors.", {
+  expect_equal(1, 1)
+})
