bfc1564e60e1e5399beef3f00bdb22a5f10eda9e
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-24 21:07:17 +0000

    updated test coverage for apply

--- tests/testthat/test-Apply.R
+++ tests/testthat/test-Apply.R
@@ -1,17 +1,23 @@
 library(functools)
 context("Apply()")
 
-x <- cbind(x1 = 3, x2 = c(4:1, 2:5))
-dimnames(x)[[1]] <- letters[1:8]
-
+x <- matrix(runif(10000), ncol = 5)
 test_that("Produces the correct output.", {
-  expect_equal(Apply(mean, x, 2, trim = 0.2), apply(x, 2, mean, trim = 0.2))
+  expect_equal(Apply(mean, x, 1), apply(x, 1, mean))
+  expect_equal(Apply(mean, x, 2), apply(x, 2, mean))
+  expect_equal(Apply(sum, x, 1), apply(x, 1, sum))
+  expect_equal(Apply(sum, x, 2), apply(x, 2, sum))
 })
 
 test_that("Produces the correct output type.", {
-  expect_is(Apply(mean, x, 2, trim = 0.2), "numeric")
+  expect_is(Apply(mean, x, 1), "numeric")
+  expect_is(Apply(mean, x, 2), "numeric")
+  expect_is(Apply(sum, x, 1), "numeric")
+  expect_is(Apply(sum, x, 2), "numeric")
+
 })
 
 test_that("Produces the correct errors.", {
-  expect_error(Apply(x, 2, mean, trim = 0.2))
+  expect_error(Apply(x, 1, mean))
+  expect_error(Apply(x, 2, mean))
 })
