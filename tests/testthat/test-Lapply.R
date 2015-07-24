b1d61281c09858d0b1fd72a07eb2c01db494d7ad
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-24 21:32:25 +0000

    update lapply test coverage

--- tests/testthat/test-Lapply.R
+++ tests/testthat/test-Lapply.R
@@ -1,14 +1,21 @@
 library(functools)
 context("Lapply()")
 
+x <- data.frame(matrix(runif(10000), ncol = 5))
 test_that("Produces the correct output.", {
-  expect_equal(Lapply(Identity, 1:10), lapply(1:10, Identity))
+  expect_equal(Lapply(Identity, x), lapply(x, Identity))
+  expect_equal(Lapply(sum, x), lapply(x, sum))
+  expect_equal(Lapply(mean, x, trim = 0.2), lapply(x, mean, trim = 0.2))
 })
 
 test_that("Produces the correct output type.", {
-  expect_is(Lapply(Identity, 1:10), "list")
+  expect_is(Lapply(Identity, x), "list")
+  expect_is(Lapply(sum, x), "list")
+  expect_is(Lapply(mean, x, trim = 0.2), "list")
 })
 
 test_that("Produces the correct errors.", {
-  expect_error(Lapply(1:10, Identity))
+  expect_error(Lapply(x, Identity))
+  expect_error(Lapply(x, sum))
+  expect_error(Lapply(x, mean, trim = 0.2))
 })
