a718321123063d7e1fd6a39bf26422db4a33d132
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-27 14:14:34 +0000

    updated with Reduce_Right

--- R/Reduce-Right.R
+++ R/Reduce-Right.R
@@ -1,13 +1,12 @@
-#' Title
+#' Simple wrapper for Reduce, proceeding from the right.
 #'
-#' Explanation
+#' Wrapper for \code{\link[base]{Reduce}} with \code{right} set to \code{TRUE}.
 #'
-#' @param x an object.
-#' @return a value.
-#' @examples
-#' # comment here
-#'
+#' @param ... Arguments to be passed to \code{\link[base]{Reduce}}.
+#' @family function operators
+#' @seealso \code{\link[base]{Reduce}} for code and documentation.
 #' @export
-Reduce_Right <- function(x) {
-  return(1L) # Placeholder
+Reduce_Right <- function(...) {
+  Reduce(..., right = TRUE)
 }
+
--- tests/testthat/test-Reduce-Right.R
+++ tests/testthat/test-Reduce-Right.R
@@ -1,14 +1,19 @@
 library(functools)
 context("Reduce_Right()")
 
+divide_x_by_y <- function(x, y) return(x / y)
+
+z <- c(128, 2, 2, 2, 2, 2, 2, 2)
+
 test_that("Produces the correct output.", {
-  expect_equal(Reduce_Right(), 1)
+  expect_equal(Reduce_Right(divide_x_by_y, z),
+               Reduce(divide_x_by_y, z, right = TRUE))
 })
 
 test_that("Produces the correct output type.", {
-  expect_is(Reduce_Right(), "integer")
+  expect_is(Reduce_Right(divide_x_by_y, z), "numeric")
 })
 
 test_that("Produces the correct errors.", {
-  expect_error(Reduce_Right()())
+  expect_error(Reduce_Right(divide_x_by_y, z)())
 })
