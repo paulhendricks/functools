965f273a02b5cdc3d3b43a69b8c28767d7902089
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-27 15:24:33 +0000

    removed stop

--- R/Memoise.R
+++ R/Memoise.R
@@ -8,10 +8,6 @@
 #' @seealso \code{\link[memoise]{memoise}} for code and documentation.
 #' @export
 Memoise <- function(f) {
-  if (!requireNamespace("memoise", quietly = TRUE)) {
-    stop("memoise needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
   return(memoise::memoise(f))
 }
 
--- R/Partial.R
+++ R/Partial.R
@@ -7,10 +7,6 @@
 #' @seealso \code{\link[pryr]{partial}} for code and documentation.
 #' @export
 Partial <- function(...) {
-  if (!requireNamespace("pryr", quietly = TRUE)) {
-    stop("pryr needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
   return(pryr::partial(...))
 }
 
